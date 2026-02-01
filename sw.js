84ea59d27d8d85cd642bd07d131102e312d2ebd8
juan <inovacion.iasa@inversionesagricolas.com.py>
2026-02-01 18:16:23 -0300

    f

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'silo-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './app.html',
+  './desechos.html'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(async (keys) => {
+      for (const key of keys) {
+        if (key !== CACHE_NAME) await caches.delete(key);
+      }
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
+});
